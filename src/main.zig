e0e40623103af330124c06d5f9ccab6f74f7f18d
zuka <55925389+zukaChachava@users.noreply.github.com>
2025-08-05 12:05:03 +0400

    Added completed status

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,44 @@
 const std = @import("std");
+const channels = @import("channels");
 
 pub fn main() !void {
-    
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    const channel = try channels.Channel(i32).init(allocator);
+    defer channel.deinit();
+
+    const writerThread = try std.Thread.spawn(.{}, writer, .{channel});
+    const readerThread = try std.Thread.spawn(.{}, reader, .{channel});
+
+    writerThread.join();
+    readerThread.join();
+}
+
+
+fn writer(channel: *channels.Channel(i32)) !void {
+    var i: i32 = 0;
+    const writerChannel = channel.getWriter();
+
+    while (i < 10) : (i += 1) {
+        try writerChannel.write(i);
+    }
+
+    writerChannel.complete();
+}
+
+fn reader(channel: *channels.Channel(i32)) void {
+    const readerChannel = channel.getReader();
+
+    while(!channel.completed){
+        const data = readerChannel.read();
+
+        if(data == null)
+            break;
+
+        std.debug.print("Received data: {}\n", .{data.?});
+    }
+
+    std.debug.print("Reading completed\n", .{});
 }
--- src/root.zig
+++ src/root.zig
@@ -9,16 +9,21 @@ pub fn Channel(comptime T: type) type{
         data: DoublyLinkedList(T),
         mutex: Thread.Mutex,
         signal: Thread.Condition,
+        completed: bool,
 
         const Self = @This();
 
-        pub fn init(allocator: Allocator) Self{
-            return .{
+        pub fn init(allocator: Allocator) !*Self{
+            const channel = try allocator.create(Self);
+            channel.* = .{
                 .allocator = allocator,
                 .data = DoublyLinkedList(T){},
                 .mutex = Thread.Mutex{},
-                .signal = Thread.Condition{}
+                .signal = Thread.Condition{},
+                .completed = false
             };
+
+            return channel;
         } 
 
         pub fn getReader(self: *Self) Reader(T){
@@ -32,6 +37,15 @@ pub fn Channel(comptime T: type) type{
                 .channel = self
             };
         }
+
+        pub fn deinit(self: *Self) void{
+            while(self.data.len != 0){
+                const node = self.data.popFirst().?;
+                self.allocator.destroy(node);
+            }
+
+            self.allocator.destroy(self);
+        }
     };
 }
 
@@ -40,15 +54,32 @@ fn Writer(comptime T: type) type {
         channel: *Channel(T),
         const Self = @This();
 
-        pub fn write(self: *Self, data: T) !void {
+        pub fn write(self: Self, data: T) !void {
             self.channel.mutex.lock();
             defer self.channel.mutex.unlock();
 
-            var node = try self.channel.allocator.create(std.DoublyLinkedList(T).Node);
-            node.data = data;
-            self.channel.data.append(node);
+            if(self.channel.completed)
+                return; // ToDo: Return Error
 
+            const node = try self.channel.allocator.create(std.DoublyLinkedList(T).Node);
+            node.* = .{
+                .data = data,
+                .next = null,
+                .prev = null
+            };
+            self.channel.data.append(node);
             self.channel.signal.signal();
+        }
+
+        pub fn complete(self: Self) void {
+            self.channel.mutex.lock();
+            defer self.channel.mutex.unlock();
+
+            if(self.channel.completed)
+                return; // ToDo: Return Error
+
+            self.channel.completed = true;
+            self.channel.signal.broadcast();
         }
     };
 }
@@ -59,11 +90,15 @@ fn Reader(comptime T: type) type{
 
         const Self = @This();
 
-        pub fn read(self: *Self) T {
+        pub fn read(self: Self) ?T {
             self.channel.mutex.lock();
             defer self.channel.mutex.unlock();
             
             while (self.channel.data.len == 0){
+
+                if(self.channel.completed)
+                    return null;
+
                 self.channel.signal.wait(&self.channel.mutex);
             }
 
